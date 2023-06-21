aaa602b51d2ed9e47ed2396c52c439f9255e4e3d
sweetypujitha <132543781+sweetypujitha@users.noreply.github.com>
2023-06-21 11:11:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web2" {
+resource "aws_instance" "web3" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 
